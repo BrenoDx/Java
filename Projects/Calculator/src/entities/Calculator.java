2283af1f02aa934166e81ab65468738b9c229832
Breno Alves <breno.alvesdx@gmail.com>
2025-04-23 18:31:12 -0300

    Fix: Projeto calculadora

---
+++ Projects/Calculator/src/entities/Calculator.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author breno
+ */
+public class Calculator {
+    
+    public double addition(double num1, double num2){
+        return num1 + num2;
+    }
+    
+    public double less(double num1, double num2){
+        return num1 - num2;
+    }
+    
+    public double multiplication(double num1, double num2){
+        return num1 * num2;
+    }
+    
+    public double division(double num1, double num2){
+        return num1 * num2;  
+    }
+}
