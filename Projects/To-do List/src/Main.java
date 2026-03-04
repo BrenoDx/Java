930b3f6d0eb7176445f87be2ebd65c7f62806447
breno <breno.alvesdx@gmail.com>
2026-03-04 20:05:16 -0300

    Feat: iniciando parte do menu (Terminal)

--- Projects/To-do List/src/Main.java
+++ Projects/To-do List/src/Main.java
@@ -2,5 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
 
+    System.out.println("============================================");
+    System.out.println("Olá, seja bem-vindo Agenda de Tarefas!");
+    System.out.println("1 - Criar; 2 - Excluir; 3 - Listar; 4 - Sair");
+    System.out.println("============================================");
+
 
 }
