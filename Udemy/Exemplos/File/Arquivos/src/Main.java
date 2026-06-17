7457e41325113e6108ab71dd4eab892b74fa5532
breno <breno.alvesdx@gmail.com>
2026-06-17 19:32:24 -0300

    Feat: Exemplo FileReader e BufferedReader

--- Udemy/Exemplos/File/Arquivos/src/Main.java
+++ Udemy/Exemplos/File/Arquivos/src/Main.java
@@ -1,6 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    /*
     Scanner sc = null;
     File file = new File("caminho");
 
@@ -14,6 +15,39 @@ void main() {
     }finally {
         if(sc != null){
             sc.close();
+        }
+    }
+
+    */
+    FileReader fr = null;
+    BufferedReader br = null;
+    String path = "caminho";
+    try{
+        /*
+        fr = new FileReader(path);
+        br = new BufferedReader(fr);
+        */
+
+        br = new BufferedReader(new FileReader(path));
+        String line = br.readLine();
+
+        while(line != null){
+            System.out.println(line);
+            line = br.readLine();
+        }
+
+    }catch (IOException e){
+        System.out.println("Error: " + e.getMessage());
+    }finally {
+        try {
+            if (br != null) {
+                br.close();
+            }
+            if (fr != null) {
+                fr.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
         }
     }
 
