e5114f3f43aedfaf79eb910005c6c3d98afeb642
breno <breno.alvesdx@gmail.com>
2026-06-29 20:59:00 -0300

    Feat: Adicionando a lógica da atv

---
+++ Udemy/File/src/entidades/Produto.java
@@ -0,0 +1,29 @@
+package entidades;
+
+public class Produto {
+    private String nome;
+    private double preço;
+    private int qtd;
+
+    public Produto( String nome, double valor, int qtd){
+        this.nome = nome;
+        this.preço = valor;
+        this.qtd = qtd;
+    }
+
+    public double total(){
+        return getPreço() * getQtd();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreço() {
+        return preço;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+}
