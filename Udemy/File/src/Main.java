5e1e641c8f2fc0e982d4a2a25b3ef1087fcb0ae0
breno <breno.alvesdx@gmail.com>
2026-06-30 20:34:33 -0300

    Feat: Finalizando atv01 bloco File

--- Udemy/File/src/Main.java
+++ Udemy/File/src/Main.java
@@ -6,6 +6,7 @@ import static java.lang.Integer.parseInt;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
     System.out.print("Caminho do arquivo");
@@ -13,14 +14,30 @@ void main() {
 
     File path = new File(strPath);
 
+    List<Produto> lista = new ArrayList<>();
+
     try(BufferedReader sb = new BufferedReader(new FileReader(path))){
         String linha = sb.readLine();
 
-        String nome = linha.split(",")[0];
-        String strValor = linha.split(",")[1];
-        String strQtd = linha.split(",")[2];
+        while(linha != null){
+            String nome = linha.split(",")[0];
+            String strValor = linha.split(",")[1];
+            String strQtd = linha.split(",")[2];
 
-        Produto p = new Produto(nome, parseDouble(strValor), parseInt(strQtd));
+            linha = sb.readLine();
+            lista.add(new Produto(nome, parseDouble(strValor), parseInt(strQtd)));
+        }
+        File caminhoGerar = new File(path.getParent()+"\\sumario.csv");
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(caminhoGerar, true))){
+            for(Produto p: lista) {
+                bw.write(p.getNome() + ", " + String.format("%.2f", p.total()));
+                bw.newLine();
+            }
+            System.out.println("Arquivo csv gerado com sucesso");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     } catch (IOException e) {
         e.printStackTrace();
     }
