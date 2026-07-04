0ab4ab228bee2a3ab55f19e53e44fe3b97c3089a
breno <breno.alvesdx@gmail.com>
2026-07-03 22:20:34 -0300

    Feat: Finalizando exemplo de interfaces

--- Udemy/Exemplos/Interfaces/Locadora/src/models/services/TaxaService.java
+++ Udemy/Exemplos/Interfaces/Locadora/src/models/services/TaxaService.java
@@ -1,11 +1,7 @@
 package models.services;
 
-public class TaxaService {
+public interface TaxaService {
 
-    public double taxa(double valor){
-        if(valor <= 100.0)
-            return valor*0.2;
-        return valor * 0.15;
-    }
+    double taxa(double valor);
 
 }
