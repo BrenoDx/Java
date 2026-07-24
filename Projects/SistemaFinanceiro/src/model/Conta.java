ef56d53a02835e8e220f6ff1dbf4d944bde5126c
breno <breno.alvesdx@gmail.com>
2026-07-23 22:31:14 -0300

    Feat: Realizando escopo inicial Conta/Service

--- Projects/SistemaFinanceiro/src/model/Conta.java
+++ Projects/SistemaFinanceiro/src/model/Conta.java
@@ -1,4 +1,25 @@
 package model;
 
 public class Conta {
+    private String numero;
+    private String titular;
+    private double saldo;
+
+    public Conta(String numero, String titular, double saldo){
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
