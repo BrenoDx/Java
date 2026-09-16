264acb0cb748b4e5af3f563ce9951cc8f737097a
breno <breno.alvesdx@gmail.com>
2026-09-16 18:29:11 -0300

    Feat: Exemplo Function, implementação da interface

---
+++ Udemy/ProgramacaoFuncional&Lambda/Exemplos/Function/src/Main.java
@@ -0,0 +1,17 @@
+import entidades.Produto;
+import util.UpperCaseName;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    Locale.setDefault(Locale.US);
+    List<Produto> list = new ArrayList<>();
+
+    list.add(new Produto("Tv", 900.00));
+    list.add(new Produto("Mouse", 50.00));
+    list.add(new Produto("Tablet", 350.50));
+    list.add(new Produto("HD Case", 80.90));
+
+    List<String> nomes = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+    nomes.forEach(System.out::println);
+}
